3d7044ce1eacaa7d3abc5f1a10493cb1da3903da
Razkanaz <azneeisdumb@gmail.com>
2023-01-22 13:25:08 +0700

    update
    -added before or after and daysinmonth

---
+++ C#/DateAndTime/BeforeOrAfter/BeforeOrAfter.cs
@@ -0,0 +1,24 @@
+﻿using Utilities;
+
+namespace DAT
+{
+    class BTTF
+    {
+        static void Main(string[] args)
+        {
+            int year, month, day;
+            int year2, month2, day2;
+            year = Util.askint("please put in a year: ");
+            month = Util.askint("Please put in a month: ");
+            day = Util.askint("Please put in a day: ");
+            year2 = Util.askint("please put in another year: ");
+            month2 = Util.askint("Please put in another month: ");
+            day2 = Util.askint("Please put in another day: ");
+
+            DateTime time1 = new DateTime(year, month, day), time2 = new DateTime(year2, month2, day2);
+            
+            Console.WriteLine(DateTime.Compare(time1, time2) == 1 ? $"{time1.ToString("dd/MM/yyyy")} is later than {time2.ToString("dd/MM/yyyy")}" : $"{time2.ToString("dd/MM/yyyy")} is later than {time1.ToString("dd/MM/yyyy")}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/DateAndTime/DaysInMonth/DaysInMonth.cs
@@ -0,0 +1,17 @@
+﻿using Utilities;
+
+namespace DAT
+{
+    class BTTF
+    {
+        static void Main(string[] args)
+        {
+            int year, month;
+            year = Util.askint("please put in a year: ");
+            month = Util.askint("Please put in a month: ");
+
+            Console.WriteLine($"There are {DateTime.DaysInMonth(year, month)} days in that month");
+            Console.ReadKey();
+        }
+    }
+}
