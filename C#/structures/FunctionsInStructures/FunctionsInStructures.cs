9c4539d7ef070fb67f81098cfb6b37f988bc274f
Razkanaz <azneeisdumb@gmail.com>
2023-02-09 16:57:37 +0700

    update

---
+++ C#/structures/FunctionsInStructures/FunctionsInStructures.cs
@@ -0,0 +1,38 @@
+﻿namespace Structures;
+
+struct Register
+{
+    private int number;
+    
+    public int n
+    {
+        get
+        {
+            return number;
+        }
+        set
+        {
+            if (value%2 == 0)
+            {
+                number = value;
+            }
+        }
+    }
+
+    public void Pr ()
+    {
+        Console.WriteLine($"number = {number}");
+    }
+}
+
+class structures
+{
+    static void Main(string[] args)
+    {
+        Register r = new Register();
+
+        r.n = 50;
+        r.Pr();
+        Console.ReadKey();
+    }
+}
---
+++ C#/structures/RefrencingStructures/RefrencingStructures.cs
@@ -0,0 +1,37 @@
+﻿namespace Structures;
+
+struct Stemp
+{
+    public int n;
+}
+
+class Ctemp
+{
+    public int n;
+}
+
+class structure
+{
+    static void ChangeS (Stemp ST)
+    {
+        ST.n = 0;
+    }
+
+    static void ChangeC(Ctemp ST)
+    {
+        ST.n = 0;
+    }
+
+    static void Main(string[] args)
+    {
+        Stemp NS = new Stemp();
+        Ctemp NC = new Ctemp();
+        NS.n = 10;
+        NC.n = 20;
+        ChangeS(NS);
+        ChangeC(NC);
+        Console.WriteLine($"NS: {NS.n}\nNC: {NC.n}");
+        Console.ReadKey();
+        
+    }
+}
