526e6555d48867b00a8bb379b6efb13a07901534
Razkanaz <azneeisdumb@gmail.com>
2023-04-15 17:15:48 +0700

    trawe

---
+++ C#/polymorphism/Runtime/Runtime.cs
@@ -0,0 +1,42 @@
+﻿namespace CriticOpinion;
+
+class Program
+{
+    public class SongReview
+    {
+        public virtual void Opinion ()
+        {
+            Console.WriteLine("I love Tylers New Album, cherry bomb");
+        }
+    }
+
+    class Fantano : SongReview
+    {
+        public override void Opinion ()
+        {
+            Console.WriteLine("I hate tylers new album");
+        }
+    }
+
+    class Tyler : SongReview
+    {
+        public override void Opinion ()
+        {
+            Console.WriteLine("I loved making My new album");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        SongReview s = new SongReview();
+        Fantano fantano = new Fantano();
+        Tyler tylertylertyler = new Tyler();
+
+        s.Opinion();
+        fantano.Opinion();
+        tylertylertyler.Opinion();
+
+        Console.ReadKey();
+    }
+
+}
--- C#/polymorphism/polymorphism/polymorphism.cs
+++ C#/polymorphism/polymorphism/polymorphism.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PolyMorph;
+
+class PolyMorph
+{
+
+    void combine (int a, int b)
+    {
+        Console.WriteLine($"Combine 2 Numbers: {a + b}");
+    }
+
+    void combine(string a, string b)
+    {
+        Console.WriteLine($"Combine 2 Words: {a + b}");
+    }
+
+    static void Main(string[] args)
+    {
+        PolyMorph m = new PolyMorph();
+        m.combine ("3", "5");
+        m.combine (3,5);
+
+        Console.ReadKey();
+    }
+}
