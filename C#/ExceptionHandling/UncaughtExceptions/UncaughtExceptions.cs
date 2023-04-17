e9268862dd3461d78be4d262c81877203e5d7a1a
Razkanaz <azneeisdumb@gmail.com>
2023-04-17 12:21:47 +0700

    dgsfagdfs

---
+++ C#/ExceptionHandling/UncaughtExceptions/UncaughtExceptions.cs
@@ -0,0 +1,31 @@
+﻿namespace Exception;
+
+using Utilities;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        int x = 0, div = 12;
+
+        x = Util.askint("Pick any number: ");
+
+		try
+		{
+            div = 100 / x;
+            Console.WriteLine($"100/{x} is {div}");
+        }
+		catch (DivideByZeroException)
+		{
+            Console.WriteLine("you can't divide by zero stupid");
+		}
+        finally
+        {
+            Console.WriteLine("Thank you For participating");
+        }
+
+        Console.WriteLine($"\noh btw {x} is a stupid number");
+
+        Console.ReadKey();
+    }
+}
